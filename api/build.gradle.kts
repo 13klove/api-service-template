903b50c29f2d7fae2de42670bd63b287bf5f70ec
hbjang <13klove@naver.com>
2021-07-18 17:29:43 +0900

    add: logging, valid jar

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,10 +7,15 @@ dependencies{
     implementation(project(":external"))
     implementation(project(":message"))
 
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("io.springfox:springfox-swagger2:2.9.2")
+    implementation("io.springfox:springfox-swagger-ui:2.9.2")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
--- domain-mysql/build.gradle.kts
+++ domain-mysql/build.gradle.kts
@@ -5,9 +5,17 @@ repositories {
 dependencies{
     implementation(project(":message"))
 
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.6")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.h2database:h2")
-    //runtimeOnly("mysql:mysql-connector-java")
+    //runtimeOnly("com.h2database:h2")
+
+    implementation("org.springframework.security:spring-security-crypto:5.3.8.RELEASE")
+
+    implementation("io.jsonwebtoken:jjwt-api:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
+    runtimeOnly("mysql:mysql-connector-java")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
